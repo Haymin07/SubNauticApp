218cedb52a93ea3182db59abac3323b064928e33
Haymin07 <majo37la@gmail.com>
2019-02-09 09:41:52 +0100

    Affichage d'une base avec toutes ses caractéristiques

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,17 @@ use Illuminate\Http\Request;
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// une base avec toutes ses caractéristiques
+$router->get('bases/{id}', function ($id) {
+	$res = DB::select("SELECT COUNT(*) AS nb FROM NauticBase WHERE id=?",[$id]);
+	if ($res[0]->nb == 0) {
+		return response()->json(["status"=>false,"message"=>"Base nautique inexistante"],200);
+	}
+	$base = DB::select("SELECT * FROM NauticBase WHERE id=?",[$id]);
+	return response()->json(["status"=>true, "base"=>$base[0]]);
+});
